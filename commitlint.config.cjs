47945202a0e6d152cad6df696cdca1674670f68d
this-wastes-time <amacmusic90@gmail.com>
2025-04-01 23:57:41 -0500

    ci: add type BREAKING CHANGE and more packages for automated release, etc

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+      ['BREAKING CHANGE', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
     ],
     'subject-case': [
       2,
